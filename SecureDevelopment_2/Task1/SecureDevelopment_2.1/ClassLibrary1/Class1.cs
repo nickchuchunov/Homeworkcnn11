de1160acd08b24f2ba181117376bd021508e3a6b
nickchuchunov <nick.chuchunov@yandex.ru>
2022-01-30 17:09:40 +0300

    Создание 3 практического задания создание строго подписанной сборки

---
+++ SecureDevelopment_2/Task1/SecureDevelopment_2.1/ClassLibrary1/Class1.cs
@@ -0,0 +1,13 @@
+﻿namespace ClassLibrary1
+{
+    public class HelloWorld
+    {
+        void Print() 
+        {
+            Console.WriteLine( " Добрый день ! Вы обратились к методу Print из класса HelloWorl ");
+        }
+
+
+
+    }
+}
